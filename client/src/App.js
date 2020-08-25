83146f9aa9e323cd96fe17408b0f8abfd8aab03b
Urinrin Ogidiama <o.urinrin@tomationng.com>
2020-08-25 19:58:56 +0100

    redux boilerplate setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React, {
   Fragment
 } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+// Layouts
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 const App = () =>
-  <Fragment >
-    <h1>App</h1> 
-  </Fragment >
+  <Provider store={store}>
+    <Router>
+      <Fragment >
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment >
+    </Router>
+  </Provider>
 
 export default App;
